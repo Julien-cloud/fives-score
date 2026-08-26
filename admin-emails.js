8891f56ea6027459b4605856345703d536121898
Julien-cloud <65771719+Julien-cloud@users.noreply.github.com>
2026-08-26 11:14:12 +0200

    Add files via upload

---
+++ admin-emails.js
@@ -0,0 +1,7 @@
+// Seules les adresses de cette liste peuvent utiliser les fonctions d'administration.
+window.FIVES_ADMIN_EMAILS = [
+  'juliencannoux@yahoo.com',
+  'juliiengravity350@gmail.com',
+  'rayanrahou51@gmail.com',
+  'lucas.nadreau@gmail.com'
+];
